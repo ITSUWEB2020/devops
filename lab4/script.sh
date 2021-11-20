894ea374f98c6b2763cbaad7a3665153177b3e4a
ITSUWEB2020 <75780802+ITSUWEB2020@users.noreply.github.com>
2021-11-20 10:29:20 +0200

    devops labs

---
+++ lab4/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo useradd adminuser
+sudo usermod -aG sudo adminuser
+
+sudo useradd poweruser
+sudo bash -c 'echo "poweruser ALL=(ALL:ALL) /sbin/iptables" >> /etc/sudoers'
+usermod -aG adminuser poweruser
+sudo find /home/ -perm /640
