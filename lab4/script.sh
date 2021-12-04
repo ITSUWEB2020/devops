28153208e28d43d44ce46c28fefc1c5a26258fa4
ITSUWEB2020 <75780802+ITSUWEB2020@users.noreply.github.com>
2021-12-04 11:47:51 +0200

    lab4 fix

--- lab4/script.sh
+++ lab4/script.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 sudo useradd adminuser
-sudo usermod -aG sudo adminuser
 
-sudo useradd poweruser
-sudo bash -c 'echo "poweruser ALL=(ALL:ALL) /sbin/iptables" >> /etc/sudoers'
-usermod -aG adminuser poweruser
-sudo find /home/ -perm /640
+sudo echo 'adminuser:secret' | sudo chpasswd
+# Create group admin
+groupadd admin
+
+echo '%admin ALL=(ALL:ALL) ALL' >> /etc/sudoers
+# Grant for “adminuser” sudoer permission
+usermod -a -G admin adminuser
+# Create user “poweruser”
+useradd poweruser
+# Grant for “poweruser” permission for iptables command
+echo 'poweruser ALL=(ALL:ALL) /usr/sbin/iptables' >> /etc/sudoers
+# Allow “poweruser” to read home directory of “adminuser” 
+usermod -a -G adminuser poweruser
+# List all of files with SUID bit set
+find / -type d -perm 4000 -exec ls -ld {} \; 1>> /var/suid_files.txt 2> /dev/null
